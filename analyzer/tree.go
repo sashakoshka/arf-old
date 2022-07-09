dab5361b509064767ea4c780bfb05f7b05e29c4d
Sasha Koshka <supergriffin64@gmail.com>
2022-07-08 21:34:06 -0400

    Semantic tree can locate sections by module and name

--- analyzer/tree.go
+++ analyzer/tree.go
@@ -2,10 +2,15 @@ package analyzer
 
 import "github.com/sashakoshka/arf/parser"
 
+type SectionSpecifier struct {
+        module string
+        name   string
+}
+
 type SemanticTree struct {
-        typedefs  map[string] *Typedef
-        datas     map[string] *Data
-        functions map[string] *Function
+        typedefs  map[SectionSpecifier] *Typedef
+        datas     map[SectionSpecifier] *Data
+        functions map[SectionSpecifier] *Function
 }
 
 type Typedef struct {
@@ -32,4 +37,58 @@ type Data struct {
 
 type Function struct {
         
+}
+
+func (tree *SemanticTree) GetTypedef (
+        module string,
+        name   string,
+) (
+        typedef *Typedef,
+) {
+        return tree.typedefs[SectionSpecifier { module: module, name: name }]
+}
+
+func (tree *SemanticTree) GetData (
+        module string,
+        name   string,
+) (
+        data *Data,
+) {
+        return tree.datas[SectionSpecifier { module: module, name: name }]
+}
+
+func (tree *SemanticTree) GetFunction (
+        module string,
+        name   string,
+) (
+        function *Function,
+) {
+        return tree.functions[SectionSpecifier { module: module, name: name }]
+}
+
+func (tree *SemanticTree) setTypedef (
+        module string,
+        name   string,
+        typedef *Typedef,
+) {
+        tree.typedefs[SectionSpecifier { module: module, name: name }] =
+                typedef
+}
+
+func (tree *SemanticTree) setData (
+        module string,
+        name   string,
+        data *Data,
+) {
+        tree.datas[SectionSpecifier { module: module, name: name }] =
+                data
+}
+
+func (tree *SemanticTree) setFunction (
+        module string,
+        name   string,
+        function *Function,
+) {
+        tree.functions[SectionSpecifier { module: module, name: name }] =
+                function
 }
